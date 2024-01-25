5381f4c7afeb845df6d05af1c814637ccb454a14
Samuel wanza <s.wanza@alustudent.com>
2024-01-25 08:12:20 +0200

    added devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
-end
+    protect_from_forgery with: :exception
+  
+    before_action :update_allowed_parameters, if: :devise_controller?
+  
+    protected
+  
+    def update_allowed_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :telephone, :email, :password, :role)}
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :telephone, :email, :password, :current_password, :role)}
+    end
+  end
